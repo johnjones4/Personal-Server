ff61db16b76542af59463f4d1157259f5e0627af
johnjones <johnjones@local.johnjonesfour.com>
2018-03-21 13:43:05 -0400

    setup changes

--- setup.sh
+++ setup.sh
@@ -4,13 +4,9 @@ mkdir plex
 mkdir plex/config
 mkdir plex/transcode
 mkdir plex/data
-mkdir gitlab
-mkdir gitlab/etc
-mkdir gitlab/log
-mkdir gitlab/opt
 mkdir doomsday-machine
 mkdir doomsday-machine/config
 mkdir doomsday-machine/files
 mkdir doomsday-machine/files/workdir
 mkdir doomsday-machine/files/archives
-docker-compose build
+sudo docker-compose build
