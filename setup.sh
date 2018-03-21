ffe5b8ce867fba938057f80fb170cc4c62f8fa88
John Jones <johnjones4@gmail.com>
2018-03-21 14:14:28 -0400

    volume tweaks

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir plex
-mkdir plex/config
-mkdir plex/transcode
-mkdir plex/data
-mkdir doomsday-machine
-mkdir doomsday-machine/config
-mkdir doomsday-machine/files
-mkdir doomsday-machine/files/workdir
-mkdir doomsday-machine/files/archives
+# mkdir plex
+# mkdir plex/config
+# mkdir plex/transcode
+# mkdir plex/data
+# mkdir doomsday-machine
+# mkdir doomsday-machine/config
+# mkdir doomsday-machine/files
+# mkdir doomsday-machine/files/workdir
+# mkdir doomsday-machine/files/archives
 sudo docker-compose build
