5d77f1d692eb0914dfa8970325076b11638e4726
John Jones <johnjones4@gmail.com>
2018-03-21 15:29:35 -0400

    backup changes

--- backup/backup.sh
+++ backup/backup.sh
@@ -3,11 +3,6 @@
 TIMESTAMP=$(date "+%Y_%m_%d")
 BACKUP_DIR="/backups"
 
-MONGO_FILE="$BACKUP_DIR/mongodb_$TIMESTAMP"
-mongodump --host=mongo--port=27017 --out="$MONGO_FILE"
-tar zcf "$MONGO_FILE.tar.gz" "$MONGO_FILE"
-rm -rf "$MONGO_FILE"
-
 ROLODEX_FILES="$BACKUP_DIR/rolodex_$TIMESTAMP"
 tar zcf "$ROLODEX_FILES.tar.gz" "/containers/rolodex"
 
@@ -16,9 +11,6 @@ tar zcf "$DOOMSDAY_MACHINE_FILES.tar.gz" "/containers/doomsday-machine"
 
 PLEX_FILES="$BACKUP_DIR/plex_$TIMESTAMP"
 tar zcf "$PLEX_FILES.tar.gz" "/containers/plex"
-
-GITLAB_FILES="$BACKUP_DIR/gitlab_$TIMESTAMP"
-tar zcf "$GITLAB_FILES.tar.gz" "/containers/gitlab"
 
 find "$BACKUP_DIR" -type f -mtime +7 -delete
 
